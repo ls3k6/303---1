d01b364416f703ce4cf0916ad6c9a4d40f79d25e
ls3k6 <90429544+ls3k6@users.noreply.github.com>
2022-09-19 20:20:50 -0500

    Update readdata.h

--- readdata.h
+++ readdata.h
@@ -3,7 +3,7 @@
 #include <fstream>
 using namespace std;
 
-int readInFile(int* &org, int &SIZE);
-int checkNum(int *org, int numSearched, int SIZE);
 void addNum(int* &org, int newNum, int &SIZE);
 void delNum(int* &org, int index, int &SIZE);
+int readInFile(int* &org, int &SIZE);
+int checkNum(int *org, int numSearched, int SIZE);
