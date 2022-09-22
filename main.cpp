436a16f858f59e31c5edeca8d9d9b4ecf77e73aa
ls3k6 <90429544+ls3k6@users.noreply.github.com>
2022-09-21 20:56:12 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,18 +7,18 @@ int main() {
   int* org;
   int SIZE, num;
   readInFile(org, SIZE);
-  for(int i = 0; i < SIZE; i++){
+  for(int i = 0; i < SIZE; i++) {
     cout << org[i] << " ";
   }
 
   cout << "\n\nNumber to check for: ";
   cin >> num;
-  if(checkNum(org, num, SIZE) == -1){
-    cout << "The number does not exist" << endl;
-    return 0;
+    if(checkNum(org, num, SIZE) == -1) {
+      cout << "The number does not exist" << endl;
+      return 0;
   }
-  else{
-    cout << "\nThe index: " << checkNum(org,num, SIZE) << endl;
+    else {
+      cout << "\nThe index: " << checkNum(org,num, SIZE) << endl;
   }
 
   cout << "\n\nNumber to add: ";
@@ -26,7 +26,7 @@ int main() {
   
   addNum(org, num, SIZE);
   
-  for(int i = 0; i < SIZE; i++){
+  for(int i = 0; i < SIZE; i++) {
     cout << org[i] << " ";
   }
 
@@ -35,7 +35,7 @@ int main() {
   
   delNum(org, num, SIZE);
 
-  for(int i = 0; i < SIZE; i++){
+  for(int i = 0; i < SIZE; i++) {
     cout << org[i] << " ";
   }
 }
