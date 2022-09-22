63d4dcd330f22a6a09dc0c4bfa369c28f09014f8
ls3k6 <90429544+ls3k6@users.noreply.github.com>
2022-09-21 20:57:36 -0500

    Update readdata.cpp

--- readdata.cpp
+++ readdata.cpp
@@ -1,35 +1,35 @@
 #include "readdata.h"
 
-int readInFile(int* &org, int &SIZE){
+int readInFile(int* &org, int &SIZE) {
   ifstream inFile;
   inFile.open("txt.txt");
-  if(!inFile.is_open()){
-    cout << "FILE NOT FOUND" << endl;
-    return 1;
+    if(!inFile.is_open()) {
+      cout << "FILE NOT FOUND" << endl;
+      return 1;
   }
 
   int num;
   SIZE = 100;
   org = new int[SIZE];
-  for(int i = 0; i < SIZE; i++){
+  for(int i = 0; i < SIZE; i++) {
     inFile >> num;
     org[i] = num;
   }
   return 0;
 }
 
-int checkNum(int *org, int numSearched, int SIZE){
-  for(int i = 0; i < SIZE; i++){
-    if(org[i] == numSearched){
+int checkNum(int *org, int numSearched, int SIZE) {
+  for(int i = 0; i < SIZE; i++) {
+    if(org[i] == numSearched) {
       return i;
     }
   }
   return -1;
 }
 
-void addNum(int* &org, int newNum, int &SIZE){
+void addNum(int* &org, int newNum, int &SIZE) {
   int* newArray = new int[SIZE+1];
-  for(int i = 0; i < SIZE; i++){
+  for(int i = 0; i < SIZE; i++) {
     newArray[i] = org[i];
   }
   newArray[SIZE] = newNum;
@@ -39,12 +39,12 @@ void addNum(int* &org, int newNum, int &SIZE){
   delete[] temp;
 }
 
-void delNum(int* &org, int index, int &SIZE){
+void delNum(int* &org, int index, int &SIZE) {
   int* newArray = new int[SIZE-1];
-  for(int i = 0; i < index; i++){
+  for(int i = 0; i < index; i++) {
     newArray[i] = org[i];
   }
-  for(int i = index+1; i < SIZE; i++){
+  for(int i = index+1; i < SIZE; i++) {
     newArray[i-1] = org[i];
   }
   SIZE--;
